30fd5e15672dc7d7a330400019a1933936704680
Jeremy <nuttapol.thita@gmail.com>
2020-06-15 16:15:24 +0700

    init sdk

---
+++ IndoorMap.framework/Headers/IndoorMap.h
@@ -0,0 +1,19 @@
+//
+//  IndoorMap.h
+//  IndoorMap
+//
+//  Created by Itthisak Phueaksri on 15/6/2563 BE.
+//  Copyright © 2563 SmartSoftAsia. Co, Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IndoorMap.
+FOUNDATION_EXPORT double IndoorMapVersionNumber;
+
+//! Project version string for IndoorMap.
+FOUNDATION_EXPORT const unsigned char IndoorMapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IndoorMap/PublicHeader.h>
+
+
