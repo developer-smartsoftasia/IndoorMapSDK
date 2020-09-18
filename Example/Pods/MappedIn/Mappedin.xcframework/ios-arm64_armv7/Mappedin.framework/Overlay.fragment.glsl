ae9bc854923037bb95e8f4d835144cc05cf96779
Jeremy <nuttapol.thita@gmail.com>
2020-09-18 12:50:13 +0700

    update podspec and example

--- Example/Pods/MappedIn/Mappedin.xcframework/ios-arm64_armv7/Mappedin.framework/Overlay.fragment.glsl
+++
@@ -1,9 +0,0 @@
-precision mediump float;
-
-uniform sampler2D u_Texture;
-
-varying vec2 v_TexCoord;
-
-void main() {
-    gl_FragColor = texture2D(u_Texture, v_TexCoord * vec2(1, -1) + vec2(0.5, 0.5));
-}
